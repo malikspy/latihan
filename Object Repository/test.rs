2fd92747053ec59d5237784f0f9a34165e49fbcc
Mukti Malik <malikmukti.ds@gmail.com>
2022-05-30 18:11:59 +0700

    First Commit

---
+++ Object Repository/test.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>test</name>
+   <tag></tag>
+   <elementGuidId>6ea95d1a-5b0e-4069-9d95-0e0b58f7f3d2</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
